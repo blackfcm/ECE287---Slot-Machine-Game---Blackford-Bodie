0177552857be154f3981022d2c5d55781fd0494d
blackfcm <caden.m.blackford@gmail.com>
2025-11-24 13:21:32 -0500

    Reuploaded Icons

---
+++ bar_rom.v
@@ -0,0 +1,16 @@
+module bar_rom(
+    input  wire        clk,
+    input  wire [13:0] addr,
+    output reg  [23:0] pixel   // 24-bit RGB
+);
+    reg [23:0] mem [0:16383];  
+
+    initial begin
+        $readmemh("bar_128x128.mem", mem);
+    end
+
+    always @(posedge clk) begin
+        pixel <= mem[addr];
+    end
+
+endmodule
---
+++ watermelon_rom.v
@@ -0,0 +1,16 @@
+module watermelon_rom(
+    input  wire        clk,
+    input  wire [13:0] addr,
+    output reg  [23:0] pixel   // 24-bit RGB
+);
+    reg [23:0] mem [0:16383];  
+
+    initial begin
+        $readmemh("watermelon_128x128.mem", mem);
+    end
+
+    always @(posedge clk) begin
+        pixel <= mem[addr];
+    end
+
+endmodule
