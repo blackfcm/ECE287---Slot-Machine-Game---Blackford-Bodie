a0846365ff9617856e6a799468d64054e3f292b1
blackfcm <caden.m.blackford@gmail.com>
2025-12-07 16:33:05 -0500

    "You Win" Message and Bet and Payout Display
    Everything appears, but need to fix bet and payout number display. Currently only shows 0,1,2.

--- vga_driver_memory.v
+++ vga_driver_memory.v
@@ -145,7 +145,7 @@ localparam NUM_SPRITES = 8;
 localparam CHAR_W = 8;
 localparam CHAR_H = 8;
 
-localparam TITLE_SCALE = 3;
+localparam TITLE_SCALE = 4;
 localparam TITLE_W = CHAR_W * TITLE_SCALE;
 localparam TITLE_H = CHAR_H * TITLE_SCALE;
 
@@ -154,12 +154,22 @@ localparam TITLE_PIX_W = TITLE_LEN * TITLE_W;
 localparam TITLE_X = (640 - TITLE_PIX_W) / 2;
 localparam TITLE_Y = (CENTER_Y / 2) - (TITLE_H / 2);
 
+// "You Win" Parameters
+localparam WIN_MSG = 8;
+localparam WIN_LEN = 8;
+localparam WIN_X = (640 - (WIN_LEN * TITLE_W)) / 2;
+localparam WIN_Y = 340;
+
 wire [23:0] sprite_pixel;
 wire [7:0] font_pixel;
 
 reg [16:0] sprite_addr;
 reg [7:0] font_ascii;
 reg [3:0] font_row;
+reg win_flag;
+
+// WIN TEXT
+reg [7:0] win_text [0:7];
 
 
 // TITLE TEXT
@@ -212,6 +222,7 @@ wire [7:0] rnd_right;
 reg  [3:0] idx_left, idx_center, idx_right;  
 
 reg key0_prev;
+reg key1_prev;
 
 reg [7:0] stop_counter;
 localparam MAX_TICKS = 8'd20; 
@@ -244,6 +255,7 @@ always @(posedge clk or negedge rst) begin
         if (key0_prev && !KEY[0]) begin
             spinning     <= 1;
             stop_counter <= 0;
+				win_flag <= 0;
         end
 
        
@@ -262,11 +274,11 @@ always @(posedge clk or negedge rst) begin
 			
 			// EARLY STOP WHEN ALL THREE ALIGN (IF NEEDED TO DEMO)
 			
-			/*if (spinning) begin
+			if (spinning) begin
 				if ((idx_left == idx_center) && (idx_center == idx_right)) begin
 					  spinning <=  0;        // stop immediately
 				 end
-			end*/
+			end
        
         // STOP COUNTER
        
@@ -304,6 +316,11 @@ always @(posedge clk or negedge rst) begin
        
         if (stop_counter >= MAX_TICKS)
             spinning <= 0;
+				
+				if((idx_left == idx_center) && (idx_center == idx_right))
+					win_flag <= 1;
+				else 
+					win_flag <= 0;
 
     end
 end
@@ -328,8 +345,16 @@ initial begin
     title_text[13] = "y";
 end
 
-
-
+initial begin
+	win_text [0] = "Y";
+	win_text [1] = "O";
+	win_text [2] = "U";
+	win_text [3] = " ";
+	win_text [4] = "W";
+	win_text [5] = "I";
+	win_text [6] = "N";
+	win_text [7] = "!";
+end
 
 
 // DRAWING and INSTATION REGs and WIREs
@@ -343,6 +368,114 @@ reg [2:0] char_x;
 reg [2:0] char_y;
 reg [16:0] addr_left, addr_center, addr_right;
 
+
+// BET AND PAYOUT
+reg [6:0] bet_amount;
+
+localparam BET_LEN = 7;
+localparam BET_X   = 20;
+localparam BET_Y   = 420;
+localparam PAY_LEN = 7;
+localparam PAY_X   = 640 - (PAY_LEN * TITLE_W) - 20;
+localparam PAY_Y   = 420;
+
+reg key1_prev_reg;
+wire key1_falling;
+
+// BET ALWAYS BLOCKS
+always @(posedge clk or negedge rst) begin
+    if (!rst)
+        key1_prev_reg <= 1;
+    else
+        key1_prev_reg <= KEY[1];
+end
+
+assign key1_falling = key1_prev_reg & ~KEY[1];
+
+always @(posedge clk or negedge rst) begin
+    if (!rst)
+        bet_amount <= 7'd1;
+    else if (key1_falling) begin
+        if (bet_amount < 7'd100)
+            bet_amount <= bet_amount + 1;
+        else
+            bet_amount <= 7'd1;
+    end
+end
+
+// PAYOUT ALWAYS BLOCKS
+reg [9:0] payout_reg;  // new register for payout
+always @(posedge clk or negedge rst) begin
+    if (!rst)
+        payout_reg <= 10'd0;            // reset payout
+    else if (win_flag)
+        payout_reg <= bet_amount * 5;   // update payout only on win
+    else if (spinning)
+        payout_reg <= 10'd0;            // reset payout when spinning starts
+end
+
+
+
+// BET DIGITS
+wire [3:0] bet_hundreds = (bet_amount >= 100) ? 1 : 0;
+wire [6:0] bet_after_h  = bet_amount - (bet_hundreds ? 100 : 0);
+
+wire [3:0] bet_tens = (bet_after_h >= 90) ? 9 :
+                      (bet_after_h >= 80) ? 8 :
+                      (bet_after_h >= 70) ? 7 :
+                      (bet_after_h >= 60) ? 6 :
+                      (bet_after_h >= 50) ? 5 :
+                      (bet_after_h >= 40) ? 4 :
+                      (bet_after_h >= 30) ? 3 :
+                      (bet_after_h >= 20) ? 2 :
+                      (bet_after_h >= 10) ? 1 : 0;
+
+wire [3:0] bet_ones = bet_after_h - (bet_tens * 10);
+
+
+// PAYOUT DIGITS
+wire [3:0] p_hundreds =
+    (payout_reg >= 500) ? 5 :
+    (payout_reg >= 400) ? 4 :
+    (payout_reg >= 300) ? 3 :
+    (payout_reg >= 200) ? 2 :
+    (payout_reg >= 100) ? 1 : 0;
+
+wire [9:0] p_after_h = payout_reg - (p_hundreds * 100);
+
+wire [3:0] p_tens = (p_after_h >= 90) ? 9 :
+                    (p_after_h >= 80) ? 8 :
+                    (p_after_h >= 70) ? 7 :
+                    (p_after_h >= 60) ? 6 :
+                    (p_after_h >= 50) ? 5 :
+                    (p_after_h >= 40) ? 4 :
+                    (p_after_h >= 30) ? 3 :
+                    (p_after_h >= 20) ? 2 :
+                    (p_after_h >= 10) ? 1 : 0;
+
+wire [3:0] p_ones = p_after_h - (p_tens * 10);
+
+reg [7:0] bet_text [0:6];     
+reg [7:0] pay_text [0:6];     
+
+always @(*) begin
+    bet_text[0] = "B";
+	 bet_text[1] = "E";
+	 bet_text[2] = "T";
+	 bet_text[3] = " ";
+    bet_text[4] = "0" + bet_hundreds;
+    bet_text[5] = "0" + bet_tens;
+    bet_text[6] = "0" + bet_ones;
+
+    pay_text[0] = "P";
+	 pay_text[1] = "A";
+	 pay_text[2] = "Y";
+	 pay_text[3] = " ";
+    pay_text[4] = "0" + p_hundreds;
+    pay_text[5] = "0" + p_tens;
+    pay_text[6] = "0" + p_ones;
+    
+end
 
 
 
@@ -363,80 +496,140 @@ weighted_picker_fsm w_right(clk,rst,start_right_pick,rnd_right,sym_right,done_ri
 // DRAWING LOGIC
 
 always @(*) begin
-    vga_color = 24'h000000;
+    vga_color   = 24'h000000;
     sprite_addr = 17'h0;
-	 font_ascii = 8'h00;
-	 font_row = 4'h0;
+    font_ascii  = 8'h00;
+    font_row    = 4'h0;
 
-    
-    addr_center = 0;
-    addr_left   = 0;
-    addr_right  = 0;
     char_index  = 0;
     char_x      = 0;
     char_y      = 0;
-	
-	if (active_pixels)
-	begin
-    // CENTER
-         if (x >= CENTER_X && x < CENTER_X + SPRITE_W &&
-				 y >= CENTER_Y && y < CENTER_Y + SPRITE_H) begin
-				 
-					  addr_center = idx_center * SPRITE_W * SPRITE_H +
-										 (y - CENTER_Y) * SPRITE_W +
-										 (x - CENTER_X);
-					  sprite_addr = addr_center;
-					  vga_color   = sprite_pixel;
-				 end
-			
 
-          // LEFT
-          else if (x >= LEFT_X && x < LEFT_X + SPRITE_W &&
-                   y >= CENTER_Y && y < CENTER_Y + SPRITE_H) begin
+    addr_left   = 0;
+    addr_center = 0;
+    addr_right  = 0;
 
-                  addr_left  = idx_left * SPRITE_W * SPRITE_H +
-                               (y - CENTER_Y) * SPRITE_W +
-                               (x - LEFT_X);
-                  sprite_addr = addr_left;
-                  vga_color   = sprite_pixel;
-              end
+    if (active_pixels) begin
 
-    
+        //-------------------------
+        // CENTER SPRITE
+        //-------------------------
+        if (x >= CENTER_X && x < CENTER_X + SPRITE_W &&
+            y >= CENTER_Y && y < CENTER_Y + SPRITE_H)
+        begin
+            addr_center = idx_center * SPRITE_W * SPRITE_H +
+                          (y - CENTER_Y) * SPRITE_W +
+                          (x - CENTER_X);
 
-          // RIGHT
-          else if (x >= RIGHT_X && x < RIGHT_X + SPRITE_W &&
-                   y >= CENTER_Y && y < CENTER_Y + SPRITE_H) begin
+            sprite_addr = addr_center;
+            vga_color   = sprite_pixel;
+        end
 
-                  addr_right = idx_right * SPRITE_W * SPRITE_H +
-                               (y - CENTER_Y) * SPRITE_W +
-                               (x - RIGHT_X);
-                  sprite_addr = addr_right;
-                  vga_color   = sprite_pixel;
-              end
+        //-------------------------
+        // LEFT SPRITE
+        //-------------------------
+        else if (x >= LEFT_X && x < LEFT_X + SPRITE_W &&
+                 y >= CENTER_Y && y < CENTER_Y + SPRITE_H)
+        begin
+            addr_left = idx_left * SPRITE_W * SPRITE_H +
+                        (y - CENTER_Y) * SPRITE_W +
+                        (x - LEFT_X);
 
-      
+            sprite_addr = addr_left;
+            vga_color   = sprite_pixel;
+        end
 
-          // TITLE
-          else if (x >= TITLE_X && x < TITLE_X + TITLE_PIX_W &&
-                   y >= TITLE_Y && y < TITLE_Y + TITLE_H) begin
+        //-------------------------
+        // RIGHT SPRITE
+        //-------------------------
+        else if (x >= RIGHT_X && x < RIGHT_X + SPRITE_W &&
+                 y >= CENTER_Y && y < CENTER_Y + SPRITE_H)
+        begin
+            addr_right = idx_right * SPRITE_W * SPRITE_H +
+                         (y - CENTER_Y) * SPRITE_W +
+                         (x - RIGHT_X);
 
-              char_index = (x - TITLE_X) / TITLE_W;
-              char_x     = ((x - TITLE_X) % TITLE_W) / TITLE_SCALE;
-              char_y     = ((y - TITLE_Y) % TITLE_H) / TITLE_SCALE;
+            sprite_addr = addr_right;
+            vga_color   = sprite_pixel;
+        end
 
-              
-              if (char_index < TITLE_LEN) begin
-                  font_ascii = title_text[char_index];
-              end else begin
-                  font_ascii = 8'h20; // space
-              end
+        //-------------------------
+        // TITLE
+        //-------------------------
+        else if (x >= TITLE_X && x < TITLE_X + TITLE_PIX_W &&
+                 y >= TITLE_Y && y < TITLE_Y + TITLE_H)
+        begin
+            char_index = (x - TITLE_X) / TITLE_W;
+            char_x     = ((x - TITLE_X) % TITLE_W) / TITLE_SCALE;
+            char_y     = ((y - TITLE_Y) % TITLE_H) / TITLE_SCALE;
 
-              font_row = char_y[2:0];
+            font_ascii = (char_index < TITLE_LEN) ? 
+                         title_text[char_index] : 8'h20;
 
-              if (font_pixel[7 - char_x])
-                  vga_color = 24'hFFFFFF; // white text
-          end
-      end
+            font_row = char_y[2:0];
+
+            if (font_pixel[7 - char_x])
+                vga_color = 24'hFFFFFF;
+        end
+
+        //-------------------------
+        // YOU WIN TEXT
+        //-------------------------
+        else if (win_flag &&
+                 x >= WIN_X && x < WIN_X + WIN_LEN * TITLE_W &&
+                 y >= WIN_Y && y < WIN_Y + TITLE_H)
+        begin
+            char_index = (x - WIN_X) / TITLE_W;
+            char_x     = ((x - WIN_X) % TITLE_W) / TITLE_SCALE;
+            char_y     = ((y - WIN_Y) % TITLE_H) / TITLE_SCALE;
+
+            font_ascii = (char_index < WIN_LEN) ?
+                         win_text[char_index] : 8'h20;
+
+            font_row = char_y[2:0];
+
+            if (font_pixel[7 - char_x])
+                vga_color = 24'hFFFF00;
+        end
+
+        // BET TEXT
+        
+        else if (x >= BET_X && x < BET_X + BET_LEN * TITLE_W &&
+                 y >= BET_Y && y < BET_Y + TITLE_H)
+        begin
+            char_index = (x - BET_X) / TITLE_W;
+            char_x     = ((x - BET_X) % TITLE_W) / TITLE_SCALE;
+            char_y     = ((y - BET_Y) % TITLE_H) / TITLE_SCALE;
+
+            font_ascii = (char_index < BET_LEN) ?
+                         bet_text[char_index] : 8'h20;
+
+            font_row = char_y[2:0];
+
+            if (font_pixel[7 - char_x])
+                vga_color = 24'hFFFFFF;
+        end
+
+        // PAYOUT TEXT
+        else if (x >= PAY_X && x < PAY_X + PAY_LEN * TITLE_W &&
+                 y >= PAY_Y && y < PAY_Y + TITLE_H)
+        begin
+            char_index = (x - PAY_X) / TITLE_W;
+            char_x     = ((x - PAY_X) % TITLE_W) / TITLE_SCALE;
+            char_y     = ((y - PAY_Y) % TITLE_H) / TITLE_SCALE;
+
+            font_ascii = (char_index < PAY_LEN) ?
+                         pay_text[char_index] : 8'h20;
+
+            font_row = char_y[2:0];
+
+            if (font_pixel[7 - char_x])
+                vga_color = 24'h00FF00;
+        end
+
+    end 
 end
+
+
 
 endmodule
